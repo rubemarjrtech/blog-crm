440f61c608fe738dae03b8d71e763236553b2dd0
rubemar jr <rubemarrocha22@gmail.com>
2024-06-15 15:35:51 -0300

    update main seeder

--- src/utils/seeders/main.seeder.ts
+++ src/utils/seeders/main.seeder.ts
@@ -1,33 +1,11 @@
 import { DataSource } from 'typeorm';
-import { Seeder, runSeeder, SeederFactoryManager } from 'typeorm-extension';
+import { Seeder, runSeeder } from 'typeorm-extension';
 import { AdminSeeder } from './admin.seeder';
 import { MemberSeeder } from './member.seeder';
-import { Post } from '../../database/models/post.model';
-import { faker } from '@faker-js/faker';
+
 export class MainSeeder implements Seeder {
-   async run(
-      dataSource: DataSource,
-      factoryManager: SeederFactoryManager,
-   ): Promise<void> {
+   async run(dataSource: DataSource): Promise<void> {
       await runSeeder(dataSource, AdminSeeder);
       await runSeeder(dataSource, MemberSeeder);
-
-      const postRepository = dataSource.getRepository(Post);
-
-      const postFactory = factoryManager.get(Post);
-
-      const members = [1];
-
-      const posts = await Promise.all(
-         Array(40)
-            .fill('')
-            .map(async () => {
-               const made = await postFactory.make({
-                  member_id: faker.helpers.arrayElement(members),
-               });
-               return made;
-            }),
-      );
-      await postRepository.save(posts);
    }
 }
