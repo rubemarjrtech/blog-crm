3063523a64df0995f084d52ef4e9e7eb6ad00ad8
rubemar jr <rubemarrocha22@gmail.com>
2024-09-19 18:49:42 -0300

    update user auth logic

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,10 +5,7 @@ import { UserLoginDTO } from '../DTOs/user.dto';
 import { BodyRequest } from './types';
 import { SessionRepository } from '../database/repositories/session.repository';
 import { SessionService } from '../services/session.service';
-import AuthService, {
-   UserPayload,
-   UserSessionPayload,
-} from '../services/auth.service';
+import AuthService, { UserPayload } from '../services/auth.service';
 
 const sessionRepository = new SessionRepository();
 const sessionService = new SessionService(sessionRepository);
@@ -22,7 +19,6 @@ export class UserController {
    ): Promise<Response> => {
       try {
          const { username, password } = req.body;
-
          const user = await this.userService.login({
             username,
             password,
@@ -38,29 +34,20 @@ export class UserController {
             user.userId,
             user.username,
          );
-
          const userPayload: UserPayload = {
             userId: user.userId,
             username: user.username,
             sessionId: session.id,
          };
-
-         const userSessionPayload: UserSessionPayload = {
-            sessionId: session.id,
-            userId: user.userId,
-            username: user.username,
-         };
-
          const accessToken = AuthService.generateUserAccessToken(userPayload);
-         const refreshToken =
-            AuthService.generateUserRefreshToken(userSessionPayload);
+         const refreshToken = AuthService.generateUserRefreshToken(userPayload);
 
-         res.cookie('accessToken', accessToken, {
+         res.cookie('userAccessToken', accessToken, {
             httpOnly: true,
             maxAge: 300000,
          });
 
-         res.cookie('refreshToken', refreshToken, {
+         res.cookie('userRefreshToken', refreshToken, {
             httpOnly: true,
             maxAge: 3.156e10,
          });
@@ -77,19 +64,19 @@ export class UserController {
 
    public logout = async (req: Request, res: Response) => {
       try {
-         const { userId } = req.decodedUser;
-         const sessionsDeleted = await sessionService.delete(userId);
+         const { userId, username } = req.decodedUser;
+         await sessionService.delete(userId, username);
 
-         res.cookie('accessToken', '', {
+         res.cookie('userAccessToken', '', {
             maxAge: 0,
             httpOnly: true,
          });
-         res.cookie('refreshToken', '', {
+         res.cookie('userRefreshToken', '', {
             maxAge: 0,
             httpOnly: true,
          });
          res.status(StatusCodes.OK).json({
-            message: `Deleted with success. Number of sessions deleted was ${sessionsDeleted}`,
+            message: 'Logged out successfully!',
          });
       } catch (error) {
          console.log(error);
